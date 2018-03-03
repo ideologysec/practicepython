b6273bdc0afefccca42f2b43f483e2d4f8e79869
Eric <eric@ideolo.gy>
2018-03-03 08:10:10 -0800

    completed exercise 1

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+name = input("Please type your name: ")
+#print("Your name is " + name)
+
+age = input("Please enter your age: ")
+age = int(age)
+
+centenarian = str(int((2017-age)+100))
+
+
+print(name+", you will be 100 years old in the year " + centenarian)
